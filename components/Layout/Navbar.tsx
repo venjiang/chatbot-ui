dd709e00f1586cae6c0549a986125a92d015072b
venjiang <venjiang@gmail.com>
2023-03-13 17:50:36 +0800

    修改标题

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -4,11 +4,8 @@ export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
-        <a
-          className="ml-2 hover:opacity-50"
-          href="https://code-scaffold.vercel.app"
-        >
-          Chatbot UI
+        <a className="ml-2 hover:opacity-50" href="https://chat.teamlint.com">
+          薇薇 AI 助手
         </a>
       </div>
     </div>
